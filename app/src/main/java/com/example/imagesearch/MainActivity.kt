0d9296f4d1214b6e245cfc241979bfa6179eeb3e
minji05 <smj95133@likelion.org>
2023-09-21 16:32:55 +0900

    [FEAT] : ⚙️ MainActivity에서 Fragment 전환 및 디자인 구현

--- app/src/main/java/com/example/imagesearch/MainActivity.kt
+++ app/src/main/java/com/example/imagesearch/MainActivity.kt
@@ -2,11 +2,53 @@ package com.example.imagesearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.replace
+import androidx.fragment.app.Fragment
+import com.example.imagesearch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var searchFragment: SearchFragment
+    private lateinit var bookmarkFragment: BookmarkFragment
+    var likedItems: ArrayList<SearchModel> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        //test
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        searchFragment = SearchFragment()
+        bookmarkFragment = BookmarkFragment()
+
+        setFragment(searchFragment)
+
+
+        binding.SearchFragmentbtn.setOnClickListener {
+            setFragment(searchFragment)
+        }
+        binding.BookmarkFragmentbtn.setOnClickListener {
+            setFragment(bookmarkFragment)
+        }
     }
-}
+
+
+
+    private fun setFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frameLayout, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+
+
+//        fun addLikedItem(item: SearchModel) {
+//            if(!likedItems.contains(item)) {
+//                likedItems.add(item)
+//            }
+//        }
+//
+//        fun removeLikedItem(item: SearchModel) {
+//            likedItems.remove(item)
+//        }
+    }
