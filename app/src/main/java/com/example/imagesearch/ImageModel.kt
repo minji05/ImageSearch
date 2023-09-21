b6fd2fb0597d92885b3879b368e8ccc3765dcd95
minji05 <smj95133@likelion.org>
2023-09-21 18:18:03 +0900

    [FEAT] : ⚙️ ImageModel 파일 구성

--- app/src/main/java/com/example/imagesearch/ImageModel.kt
+++ app/src/main/java/com/example/imagesearch/ImageModel.kt
@@ -1,4 +1,36 @@
 package com.example.imagesearch
+import com.google.gson.annotations.SerializedName
 
-class ImageModel {
+data class ImageModel(
+    @SerializedName("meta")
+    val imgmeta: imgMeta,
+
+
+    @SerializedName("documents")
+    val imgdocument: ArrayList<imgDocument>
+) {
+
+    data class imgMeta(
+        @SerializedName("total_count")
+        val totalCount: Int,
+        @SerializedName("pageable_count")
+        val pageableCount: Int,
+        @SerializedName("is_end")
+        val isEnd: Boolean
+    )
+
+    data class imgDocument(
+        val collection: String,
+        @SerializedName("thumbnail_url")
+        val thumbnailUrl: String,
+        @SerializedName("image_url")
+        val imageUrl: String,
+        val width: Int,
+        val height: Int,
+        @SerializedName("display_sitename")
+        val displaySiteName: String,
+        @SerializedName("doc_url")
+        val docUrl: String,
+        val datetime: String
+    )
 }
